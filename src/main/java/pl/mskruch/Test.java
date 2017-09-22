47eb18eecd4b1c6abd05f8eb89d8a30afed61dc8
Marcin Skruch <m.skruch@smartrecruiters.com>
2017-09-22 13:31:04 +0200

    Add login

--- src/main/java/pl/mskruch/Test.java
+++ src/main/java/pl/mskruch/Test.java
@@ -1,5 +1,8 @@
 package pl.mskruch;
 
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +12,22 @@ import java.io.IOException;
 public class Test extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("totoro");
+        resp.getWriter().println("<h3>ping</h3>");
+        UserService userService = UserServiceFactory.getUserService();
+
+        String thisUrl = req.getRequestURI();
+
+        resp.setContentType("text/html");
+        if (req.getUserPrincipal() != null) {
+            resp.getWriter().println("<p>Hello, "
+                    + req.getUserPrincipal().getName()
+                    + "!  You can <a href=\""
+                    + userService.createLogoutURL(thisUrl)
+                    + "\">sign out</a>.</p>");
+        } else {
+            resp.getWriter().println("<p>Please <a href=\""
+                    + userService.createLoginURL(thisUrl)
+                    + "\">sign in</a>.</p>");
+        }
     }
 }
