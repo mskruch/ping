d886cace7a528d4de97a53e7a9412f1c64557e07
Marcin Skruch <mskruch@gmail.com>
2017-09-23 23:34:39 +0200

    Extend Check entity

--- src/main/java/pl/mskruch/data/Check.java
+++ src/main/java/pl/mskruch/data/Check.java
@@ -1,8 +1,23 @@
 package pl.mskruch.data;
 
 import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+import java.util.Date;
 
 @Entity
 public class Check
 {
+	@Id
+	Long id;
+	@Index
+	String ownerEmail;
+
+	Date created;
+	String url;
+	// Boolean active;
+
+	// Date lastCheck;
+	// Long secondsDown;
 }
