be5ac8fd44a9608f3701e095f99d837d0e08b3b8
Marcin Skruch <m.skruch@smartrecruiters.com>
2017-10-04 17:28:27 +0200

    Introduce task queues

---
+++ src/main/groovy/pl/mskruch/ping/engine/PingController.groovy
@@ -0,0 +1,71 @@
+package pl.mskruch.ping.engine
+
+import com.google.appengine.api.mail.MailService
+import com.google.appengine.api.mail.MailServiceFactory
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import pl.mskruch.ping.check.Check
+import pl.mskruch.ping.check.ChecksRoot
+import pl.mskruch.ping.service.Mailgun
+import pl.mskruch.ping.service.Pinger
+import pl.mskruch.ping.service.Result
+
+import java.util.logging.Logger
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET
+
+@Controller
+class PingController
+{
+	public static
+	final String SENDER = "noreply@czasowki-feeder.appspotmail.com";
+
+	static Logger logger = Logger.getLogger(PingController.class.getName());
+
+	ChecksRoot checks
+
+	PingController(ChecksRoot checks)
+	{
+		this.checks = checks
+	}
+
+	@RequestMapping(value = "/ping/{id}", method = GET)
+	@ResponseBody
+	def processSingleCheck(@PathVariable("id") Long id)
+	{
+		logger.info("processing " + id)
+
+		def check = checks.get(id)
+
+		Pinger pinger = new Pinger();
+		Result result = pinger.ping(check.getUrl());
+		logger.fine("ping " + check.getUrl() + " " + result.status());
+		boolean updated = checks.update(check, result);
+		if (updated) {
+			logger.info(
+					"status changed, sending notification, result: " + result);
+			notify(check, result);
+		}
+	}
+
+	private void notify(Check check, Result result) throws IOException
+	{
+		Mailgun mailgun = new Mailgun();
+		try {
+			String durationString = result.elapsedInMilliseconds() != null ? (result.elapsedInMilliseconds() / 1000) + " seconds" : "";
+			String name = check.getName() != null ? check.getName()
+					: check.getUrl();
+			mailgun.send(check.getOwnerEmail(), name + " is " + result.status(),
+					check.getUrl() + " is " + result.status() + "\n"
+							+ "Status code: " + result.responseCode() + "\n" + "Time: "
+							+ durationString + "\n" + "Details: " + result.message());
+		} catch (Exception e) {
+			logger.severe("unable to send notification: " + e.getMessage());
+			MailServiceFactory.getMailService()
+					.sendToAdmins(new MailService.Message(SENDER, null,
+					"Unable to send notification", e.getMessage()));
+		}
+	}
+}
--- src/main/groovy/pl/mskruch/servlet/PingServlet.java
+++ src/main/groovy/pl/mskruch/servlet/PingServlet.java
@@ -9,18 +9,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.appengine.api.mail.MailService;
-import com.google.appengine.api.mail.MailServiceFactory;
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskOptions;
+
 import pl.mskruch.ping.check.Check;
-import pl.mskruch.ping.check.Checks;
 import pl.mskruch.ping.check.ChecksRoot;
-import pl.mskruch.ping.service.Mailgun;
-import pl.mskruch.ping.service.Pinger;
-import pl.mskruch.ping.service.Result;
 
 public class PingServlet extends HttpServlet
 {
-	public static final String SENDER = "noreply@czasowki-feeder.appspotmail.com";
 	static Logger logger = Logger.getLogger(PingServlet.class.getName());
 
 	@Override
@@ -33,38 +30,10 @@ public class PingServlet extends HttpServlet
 		List<Check> all = checks.all();
 		logger.fine(all.size() + " checks found");
 
-		Pinger pinger = new Pinger();
-
+		Queue queue = QueueFactory.getDefaultQueue();
 		for (Check check : all) {
-			Result result = pinger.ping(check.getUrl());
-			logger.fine("ping " + check.getUrl() + " " + result.status());
-			boolean updated = checks.update(check, result);
-			if (updated) {
-				logger.info(
-					"status changed, sending notification, result: " + result);
-				notify(check, result);
-			}
-		}
-	}
-
-	private void notify(Check check, Result result) throws IOException
-	{
-		Mailgun mailgun = new Mailgun();
-		try {
-			String durationString = result.elapsedInMilliseconds() != null
-				? (result.elapsedInMilliseconds() / 1000) + " seconds"
-				: "";
-			String name = check.getName() != null ? check.getName()
-				: check.getUrl();
-			mailgun.send(check.getOwnerEmail(), name + " is " + result.status(),
-				check.getUrl() + " is " + result.status() + "\n"
-					+ "Status code: " + result.responseCode() + "\n" + "Time: "
-					+ durationString + "\n" + "Details: " + result.message());
-		} catch (Exception e) {
-			logger.severe("unable to send notification: " + e.getMessage());
-			MailServiceFactory.getMailService()
-				.sendToAdmins(new MailService.Message(SENDER, null,
-					"Unable to send notification", e.getMessage()));
+			queue.addAsync(TaskOptions.Builder.withUrl("/ping/" + check.getId())
+				.method(TaskOptions.Method.GET));
 		}
 	}
 
