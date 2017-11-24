e5213575bd9abda2354279a1ea29129b10614a34
Marcin Skruch <m.skruch@smartrecruiters.com>
2017-11-24 20:45:59 +0100

    Introduce ReactJS

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const root = path.resolve(__dirname, 'src/main/webapp');
+const src = path.resolve(root, 'javascript');
+const dest = path.resolve(__dirname, 'src/main/webapp/dist');
+
+
+module.exports = {
+    entry: src + '/index.jsx',
+    output: {
+        filename: 'bundle.js',
+        path: dest
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['react']
+                    }
+                }
+            }
+        ]
+    }
+};
