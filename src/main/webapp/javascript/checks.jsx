f46e497df2b91f262604795ef9d4ceb52b940b8c
Marcin Skruch <m.skruch@smartrecruiters.com>
2017-12-08 22:31:55 +0100

    Fix url regex

--- src/main/webapp/javascript/checks.jsx
+++ src/main/webapp/javascript/checks.jsx
@@ -4,24 +4,30 @@ import Check from "./checks/check";
 
 export default class Checks extends Component {
     render() {
-        let checks = this.props.ready ?
-            this.props.checks.map((check, i) =>
-                <Check key={check.id} number={i + 1} {...check}
-                       deleteCheck={this.props.deleteCheck}
-                       updateCheck={this.props.updateCheck}
-                       admin={this.props.admin}
-                       select={this.props.select}
-                       selected={this.props.selected && this.props.selected.id === check.id}
-                       check={check}/>
-            ) :
-            <div className="card mb-3">
-                <div className="card-body text-center"><img
-                    src="/images/ajax-loader.gif"/></div>
-            </div>;
+        if (!this.props.ready) {
+            return (
+                <div className="container">
+                    <div className="card mb-3">
+                        <div className="card-body text-center"><img
+                            src="/images/ajax-loader.gif"/></div>
+                    </div>
+                </div>
+            );
+        }
+
+        let checks = this.props.checks.map((check, i) =>
+            <Check key={check.id} number={i + 1} {...check}
+                   deleteCheck={this.props.deleteCheck}
+                   updateCheck={this.props.updateCheck}
+                   admin={this.props.admin}
+                   select={this.props.select}
+                   selected={this.props.selected && this.props.selected.id === check.id}
+                   check={check}/>);
 
         return (
             <div className="container">
                 {checks}
-            </div>);
+            </div>
+        );
     }
 }
